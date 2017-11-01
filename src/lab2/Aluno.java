ad15b0af7829bdc98ffaee94924a6e52d14c356a
117110318 ARTHUR DE LIMA FERRAO <arthurlf@lcc2-26.lcc.ufcg.edu.br>
2017-11-01 11:04:18 -0300

    bonus 5.4

--- src/lab2/Aluno.java
+++ src/lab2/Aluno.java
@@ -1,5 +1,6 @@
 package lab2;
 
+import java.util.HashMap;
 import java.util.ArrayList;
 
 /**
@@ -8,23 +9,87 @@ import java.util.ArrayList;
  */
 public class Aluno {
 
-    private ArrayList<ContaLaboratorio> contasLab;
-    private ArrayList<Disciplina> disciplinas;
-    private ArrayList<ContaCantina> contasCantina;
+    private HashMap<String, ContaLaboratorio> contasLab;
+    private HashMap<String, Disciplina> disciplinas;
+    private HashMap<String, ContaCantina> contasCantina;
     private Saude saude;
-     
-    public void cadastraLaboratorio(String nomeLaboratorio){
-        ContaLaboratorio conta = new ContaLaboratorio(nomeLaboratorio);
-        this.contasLab.add(conta);
+
+    public Aluno() {
+        this.contasLab = new HashMap<String, ContaLaboratorio>();
+        this.disciplinas = new HashMap<String, Disciplina>();
+        this.contasCantina = new HashMap<String, ContaCantina>();
+        this.saude = new Saude();
     }
-    
-    
-    public void cadastraLaboratorio(String nomeLaboratorio, int cota){
-        ContaLaboratorio conta = new ContaLaboratorio(nomeLaboratorio,cota);
-        this.contasLab.add(conta);
+
+    public void cadastraLaboratorio(String nomeLaboratorio) {
+        this.contasLab.put(nomeLaboratorio, new ContaLaboratorio(nomeLaboratorio));
     }
-    
-    public void consomeEspaco(String nomeLaboratorio, int mbytes){
-        
+
+    public void cadastraLaboratorio(String nomeLaboratorio, int cota) {
+        this.contasLab.put(nomeLaboratorio, new ContaLaboratorio(nomeLaboratorio, cota));
+    }
+
+    public void consomeEspaco(String nomeLaboratorio, int mbytes) {
+        this.contasLab.get(nomeLaboratorio).consomeEspaco(mbytes);
+    }
+
+    public void liberaEspaco(String nomeLaboratorio, int mbytes) {
+        this.contasLab.get(nomeLaboratorio).liberaEspaco(mbytes);
+    }
+
+    public boolean atingiuCota(String nomeLaboratorio) {
+        return this.contasLab.get(nomeLaboratorio).atingiuCota();
+    }
+
+    public String laboratorioToString(String nomeLaboratorio) {
+        return this.contasLab.get(nomeLaboratorio).toString();
+    }
+
+    public void cadastraDisciplina(String nomeDisciplina) {
+        this.disciplinas.put(nomeDisciplina, new Disciplina(nomeDisciplina));
+    }
+
+    public void cadastraHoras(String nomeDisciplina, int horas) {
+        this.disciplinas.get(nomeDisciplina).cadastraHoras(horas);
+    }
+
+    public void cadastraNota(String nomeDisciplina, int nota, double valorNota) {
+        this.disciplinas.get(nomeDisciplina).cadastraNota(nota, valorNota);
+    }
+
+    public boolean aprovado(String nomeDisciplina) {
+        return this.disciplinas.get(nomeDisciplina).aprovado();
+    }
+
+    public String disciplinaToString(String nomeDisciplina) {
+        return this.disciplinas.get(nomeDisciplina).toString();
+    }
+
+    public void cadastraCantina(String nomeCantina) {
+        this.contasCantina.put(nomeCantina, new ContaCantina(nomeCantina));
+    }
+
+    public void cadastraLanche(String nomeCantina, int qtdItens, int valorCentavos) {
+        this.contasCantina.get(nomeCantina).cadastraLanche(qtdItens, valorCentavos);
+    }
+
+    public void pagarConta(String nomeCantina, int valorCentavos) {
+        this.contasCantina.get(nomeCantina).pagaConta(valorCentavos);
+    }
+
+    public String cantinaToString(String nomeCantina) {
+        return this.contasCantina.get(nomeCantina).toString();
+    }
+
+    public void defineSaudeMental(String valor) {
+        this.saude.defineSaudeMental(valor);
+    }
+
+    public void defineSaudeFisica(String valor) {
+        this.saude.defineSaudeFisica(valor);
+    }
+
+    public String geral() {
+        return this.saude.toString();
     }
 }
