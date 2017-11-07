f4e0418a428c7f2f0a30b77e807ace0182c570f8
ArthurFerrao <arthurdeferrao@gmail.com>
2017-11-06 22:10:04 -0300

    java doc

--- src/lab2/Saude.java
+++ src/lab2/Saude.java
@@ -1,18 +1,21 @@
 package lab2;
 
 /**
- *
+ * Reune o cunjunto de caracteristicas referente a saúde do aluno
  * @author arthurlf
  */
 public class Saude {
-
+   /**
+   * 
+   * 
+   */
     private String saudeFisica;
     private String saudeMental;
     private String saudeGeral;
     private String emoji;
 
     /**
-     *
+     *Constrói o objeto saude definindo seus três atributos como "boa"
      */
     public Saude() {
         this.saudeFisica = "boa";
@@ -21,8 +24,9 @@ public class Saude {
     }
 
     /**
-     *
-     * @param valor
+     *Atribui o valor a saudeMental
+     *Modifica a saudeGeral chamando o metodo checaGeral
+     * @param valor conteudo da saude mental do aluno
      */
     public void defineSaudeMental(String valor) {
         this.saudeMental = valor;
@@ -30,14 +34,18 @@ public class Saude {
     }
 
     /**
-     *
-     * @param valor
+     *Atribui o valor a saudeFisica
+     *Modifica a saudeGeral chamando o metodo checaGeral
+     * @param valor conteudo da saude fisica do aluno
      */
     public void defineSaudeFisica(String valor) {
         this.saudeFisica = valor;
         checaGeral();
     }
-
+    
+    /**
+     *Modifica a saudeGeral dependendo dos valores atribuidos a saudeMental e saudeFisica
+     */
     private void checaGeral() {
         if (this.saudeFisica.equals("boa") && this.saudeMental.equals("boa")) {
             this.saudeGeral = "boa";
@@ -49,8 +57,9 @@ public class Saude {
     }
 
     /**
-     *
-     * @param valor
+     *Atribui uma string ao atributo emoji
+     *Atribui essa string a saudeGeral
+     * @param valor conteudo atribuido a emoji
      */
     public void definirEmoji(String valor) {
         this.emoji = valor;
@@ -59,7 +68,7 @@ public class Saude {
 
     /**
      *
-     * @return
+     * @return o valor de saudeGeral
      */
     public String geral() {
         return this.saudeGeral;
